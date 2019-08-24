f3881a6ba34ae95fb4dbd43c5b76785c84df4b6b
Walter Gottlieb <walter@bizequity.com>
2019-08-24 02:25:20 -0400

    Added more tests to the test suite

--- testSuite.js
+++ testSuite.js
@@ -6,29 +6,62 @@ var clearBoard = function() {
 	}
 }
 
-var printBoard = function() {
-	game.board.forEach(function(item){
-		console.log(item);
-	});
+var resetGame = function() {
+	clearBoard();
+	game.setup();
 }
 
-game.setup();
+// Following are some test cases. Uncomment them to run them.
 
-//Moving
-console.log(game.makeMove(game.BLACK, "c3", "NE").message);
-console.log(game.makeMove(game.RED, "f6", "SW").message);
-//Forced captures
-console.log(game.makeMove(game.BLACK, "b2", "NE"));
-console.log(game.makeMove(game.BLACK, "d4", "NE"));
-console.log(game.makeMove(game.RED, "d6", "SW"));
-console.log(game.makeMove(game.RED, "e7", "SE"));
-//Multiple jumps
-clearBoard();
-game.board[0][1] = game.REDMAN;
-game.board[7][0] = game.BLACKMAN;
-game.board[6][1] = game.REDMAN;
-game.board[4][3] = game.REDMAN;
-console.log(game.makeMove(game.BLACK, "a1", "NE"));
-console.log(game.makeMove(game.RED, "d4", "SW"));
-console.log(game.makeMove(game.BLACK, "a3", "NW"));
-console.log(game.makeMove(game.BLACK, "a3", "NE"));
+// //Input validation
+// resetGame();
+// console.log(game.makeMove(game.BLACK, "d", "NE"));
+// console.log(game.makeMove(game.BLACK, "c3 ", "NE"));
+// console.log(game.makeMove(game.BLACK, "c3", "northeast"));
+//
+// //Legal move selection
+// resetGame();
+// console.log(game.makeMove(game.BLACK, "d4", "NE"));
+// console.log(game.makeMove(game.BLACK, "f6", "NE"));
+// console.log(game.makeMove(game.BLACK, "c3", "SE"));
+// console.log(game.makeMove(game.RED, "f6", "SW"));
+//
+// //Non-capturing moves
+// resetGame();
+// console.log(game.makeMove(game.BLACK, "c3", "NE"));
+// console.log(game.makeMove(game.RED, "f6", "SW"));
+//
+// //Capturing moves and forced captures
+// resetGame();
+// game.makeMove(game.BLACK, "c3", "NE");
+// game.makeMove(game.RED, "f6", "SW");
+// console.log(game.makeMove(game.BLACK, "b2", "NE"));
+// console.log(game.makeMove(game.BLACK, "d4", "NE"));
+// console.log(game.makeMove(game.RED, "d6", "SW"));
+// console.log(game.makeMove(game.RED, "e7", "SE"));
+//
+// //Multiple jumps
+// clearBoard();
+// game.board[0][1] = game.REDMAN;
+// game.board[7][0] = game.BLACKMAN;
+// game.board[6][1] = game.REDMAN;
+// game.board[4][3] = game.REDMAN;
+// console.log(game.makeMove(game.BLACK, "a1", "NE"));
+// console.log(game.makeMove(game.RED, "d4", "SW"));
+// console.log(game.makeMove(game.BLACK, "a3", "NW"));
+// console.log(game.makeMove(game.BLACK, "a3", "NE"));
+// console.log(game.makeMove(game.RED, "b8", "SE"));
+//
+// //"King Me"
+// clearBoard();
+// game.board[0][1] = game.REDMAN;
+// game.board[1][2] = game.BLACKMAN;
+// game.board[3][4] = game.REDMAN;
+// console.log(game.getGameState());
+// console.log(game.makeMove(game.BLACK, "c7", "NE"));
+// console.log(game.makeMove(game.RED, "b8", "SE"));
+//
+// //King movement
+// console.log(game.makeMove(game.BLACK, "d8", "SW"));
+// console.log(game.makeMove(game.RED, "e5", "SW"));
+// console.log(game.makeMove(game.BLACK, "b6", "SE"));
